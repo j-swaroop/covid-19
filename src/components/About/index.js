f36acd3ced9dfd42009f6eeab80a72c4e50180e0
j-swaroop <jyothiswaroop654@gmail.com>
2023-10-02 14:47:06 +0000

    first

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import {Component} from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
+import './index.css'
+
+
+class About extends Component{
+    render(){
+        return(
+            <>
+                <Header />
+                <div></div>
+            </>
+        )
+    }
+}
+
+export default About
---
+++ src/components/LoaderView/index.js
@@ -0,0 +1,12 @@
+import Loader from 'react-loader-spinner'
+
+
+const LoaderView = () => {
+    return(
+        <div className='loader-container'>
+            <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        </div>
+    )
+}
+
+export default LoaderView
